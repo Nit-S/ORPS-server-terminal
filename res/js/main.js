f89f2e9aaeec1b362c8a7484acc0cad3e200303b
nitigya sharma <nitigya.sharma12@gmail.com>
2017-08-06 03:48:19 +0530

    java scriptreverted to previous working condition

--- res/js/main.js
+++ res/js/main.js
@@ -75,7 +75,6 @@ function updateParking() {
 }
 
 
-
 function getBooking(){
 	var custname=document.getElementById("custname").value;
 	var custnum=document.getElementById("custnum").value;
@@ -112,26 +111,10 @@ function getBooking(){
 	getbook.open("POST","getBooking.php",true);
 	getbook.onreadystatechange = function(){
 		if(this.readyState == 4 && this.status == 200){
-			if(getbook.responseText.trim()==="negative"){
-				alert("booking full plz try later");
-			}else{
 			var bookqr = JSON.parse(getbook.responseText);
-			// var chkinqrdata= urlencode('{"key":"'.bookqr.reg_no.'","type":"chkin"}');
-			// var chkoutqrdata= urlencode('{"key":"'.bookqr.reg_no.'","type":"chkout"}');
-			// var qrpage='<body align="center">'.
-			// 		'<h1>Registration no - '.bookqr.reg_no.'</h1>'.
-			// 		'<input type="button" value="Print The Qr Codes" onClick="window.print();window.close();">'.
-			// 		'<div id="checkin"><h3>CheckIn QR CODE</h3>'.
-			// 		'<img src="https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl='.chkinqrdata.'&choe=UTF-8" title="CheckIn"/>'.
-			// 		'</div>'.
-			// 		'<div id="CheckOut">'.
-			// 		'<h3>CheckOut QR CODE</h3>'.
-			// 		'<img src="https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl='.chkoutqrdata.'&choe=UTF-8" title="checkout" />'.
-			// 		'</div>'.
-			// 		'</body>';
-
-			var myWindow = window.open("", "QRWin", "width=600,height=750,top=25,left=500");
-            myWindow.document.write(qrpage);
+			document.getElementById("demo").innerHTML = getbook.responseText;
+			var myWindow = window.open("", "MsgWindow", "width=200,height=100");
+            myWindow.document.write("<p>"+getbook.responseText+"</p>");
 
 
     document.getElementById("custname").value="";
@@ -142,7 +125,7 @@ function getBooking(){
 	document.getElementById("vehiclename").value="";
 	document.getElementById("vehiclecolor").value="";
 
-}
+
 
 		}
 	}
@@ -165,47 +148,11 @@ function reset(){
 
 }
 
-
-
-function scanQR(){
+function scan(){
 	document.getElementById('preview').style.display='block';
 	 let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
       scanner.addListener('scan', function (content) {
-      	// task to be done on scanning the code
-		console.log(content+"  kar raha hai");
-		// var data=JSON.parse(content);
-		
-		// var scanner = new XMLHttpRequest();
-		// if(data.type==="chkin"){
-  //   	scanner.open('POST','chkin.php',true);
-  //   	}else if(data.type==="chkout"){
-  //       scanner.open('POST','chkout.php',true);
-  //   	}else{
-  //   	alert('response error');
-  //   	}
-  //   	scanner.onreadystatechange = function(){
-  //   	if(this.readyState == 4 && this.status == 200){
-  //   		if(scanner.responseText.trim()==="possitive"){
-  //   			if(data.type==="chkin"){
-  //   			alert("checked-in");
-  //   			}else if(data.type==="chkout"){
-  //       		alert("checked-out");
-  //   			}else{
-  //   			alert('response error for chk');
-  //   			}
-  //   		}else if(scanner.responseText.trim()==="negative"){
-  //   			alert("already checked in,checked-out or booking cancelled");
-  //   		}else{
-  //   			alert("response error");
-  //   		}
-  //   	}
-  //   }
-  //   	scanner.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		// scanner.send("key=".data.key."&emp=".user);
-
-
-
-
+        console.log(content+" kya dekh rahi ho");
       });
       Instascan.Camera.getCameras().then(function (cameras) {
         if (cameras.length > 0) {
